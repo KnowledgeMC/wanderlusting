d5ea7079c340fac45e6a3c8398595d1739be5a6a
Roy Hwang <tupaclypse@gmail.com>
2016-10-31 19:59:54 -0700

    Sign Up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Successfully signed up! Welcome to Wanderlusting!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_cofirmation)
+    end
 end
