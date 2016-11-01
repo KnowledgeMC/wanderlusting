016732c8712bde0abce6183919c0a8ab56fe1c0d
Roy Hwang <tupaclypse@gmail.com>
2016-11-01 09:50:43 -0700

    Loggin in and Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
-  root                'pages#home'
-  get 'help'    =>    'pages#help'
-  get 'about'   =>    'pages#about'
-  get 'contact' =>    'pages#contact'
-  get 'signup'  =>    'users#new'
+  root                 'pages#home'
+  get 'help'     =>    'pages#help'
+  get 'about'    =>    'pages#about'
+  get 'contact'  =>    'pages#contact'
+  get 'signup'   =>    'users#new'
+
+  get 'login'    =>    'sessions#new'
+  post 'login'   =>    'sessions#create'
+  delete 'logout'=>    'sessions#destroy'
 
   resources :users
 
