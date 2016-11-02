d31c1200744361d89371d32fcae4b11c60b8ca44
Roy Hwang <tupaclypse@gmail.com>
2016-11-02 15:59:03 -0700

    removing migration

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
 
-  has_attached_file :picture, styles: { large: "600x600", medium: "300x300", thumb: "150x150#"}
+  has_attached_file :picture, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#"}
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 
   validates :user_id, presence: true
